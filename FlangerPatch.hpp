9f21c7b9efc0689ac8a5fa401b32406a085438ea
Giom <guillaume.lenost@gmail.com>
2013-11-18 23:09:34 +0000

    Flangerpatch: refactored, deleted unused variables, tweaked rate range.

--- FlangerPatch.hpp
+++ FlangerPatch.hpp
@@ -5,59 +5,45 @@
 #include "CircularBuffer.hpp"
 #include "math.h"
 
-#define REQUEST_BUFFER_SIZE 1024
+#define FLANGER_BUFFER_SIZE 1024
 
 class FlangerPatch : public Patch {
 private:
-  CircularBuffer delayBuffer;
-    float rate, depth, wetDry, feedback, phase;
-    unsigned int olddelaySamples = 0, dSamples;
-
+    CircularBuffer delayBuffer;
+    float rate, depth, feedback, phase;
+    
 public:
   FlangerPatch(){
-    AudioBuffer* buffer = createMemoryBuffer(1, REQUEST_BUFFER_SIZE);
+    AudioBuffer* buffer = createMemoryBuffer(1, FLANGER_BUFFER_SIZE);
     delayBuffer.initialise(buffer->getSamples(0), buffer->getSize());
-      registerParameter(PARAMETER_A, "Rate");
-      registerParameter(PARAMETER_B, "Depth");
-      registerParameter(PARAMETER_C, "Feedback");
-      registerParameter(PARAMETER_D, "Dry/Wet");
+    registerParameter(PARAMETER_A, "Rate");
+    registerParameter(PARAMETER_B, "Depth");
+    registerParameter(PARAMETER_C, "Feedback");
+    phase = 0;
   }
-    
-    float modulate(float rate) {
-        
-        float output;
-        
-        if ( phase >= 1.0 ) phase -= 1.0;
-        phase += (1./(7200/rate));
-        output = sin(phase*(2*M_PI));    //sine function
-        
-        return output;
-    };
-
+  float modulate(float rate) {
+    phase += rate;
+    if ( phase >= 1.0 ) {
+        phase -= 1.0;
+    }
+    return sinf(phase*(2*M_PI));    //sine function
+  };
   void processAudio(AudioBuffer &buffer){
     int size = buffer.getSize();
-    float y;
     unsigned int delaySamples;
       
-    rate     = getParameterValue(PARAMETER_A);
+    rate     = getParameterValue(PARAMETER_A) * 0.000005f; // flanger needs slow rate
     depth    = getParameterValue(PARAMETER_B);
-    feedback = getParameterValue(PARAMETER_C);
-    wetDry   = getParameterValue(PARAMETER_D);
-    rate *= 0.1f;
+    feedback = getParameterValue(PARAMETER_C)* 0.707; // so we keep a -3dB summation of the delayed signal
       
-      for (int ch = 0; ch<buffer.getChannels(); ++ch) {
-          
-          for (int i = 0 ; i < size; i++) {
-              
-              float* buf = buffer.getSamples(ch);
-              
-              delaySamples = (depth * modulate(rate)) * (delayBuffer.getSize()-1);
-              y = buf[i] + feedback * delayBuffer.read(delaySamples);
-              buf[i] = buf[i] * (1.f - wetDry)+ wetDry * y;
-              delayBuffer.write(buf[i]);
-          }
-      }
-      olddelaySamples = delaySamples;
+    for (int ch = 0; ch<buffer.getChannels(); ++ch) {
+        for (int i = 0 ; i < size; i++) {
+            float* buf = buffer.getSamples(ch);
+            delaySamples = (depth * modulate(rate)) * (delayBuffer.getSize()-1); // compute delay according to rate and depth
+            buf[i] += feedback * delayBuffer.read(delaySamples); // add scaled delayed signal to dry signal
+            delayBuffer.write(buf[i]); // update delay buffer
+        }
+    }
   }
     
 };
