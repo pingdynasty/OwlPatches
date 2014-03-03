748f6f625e6096d05b405b61d9e998e4f8c4b911
Giom <guillaume.lenost@gmail.com>
2014-03-03 14:37:41 +0000

    commented AutoTalent patch

--- includes.h
+++ includes.h
@@ -1,40 +1,34 @@
 #include "GainPatch.hpp"
-
-#include "Autotalent/AutotalentPatch.hpp"
-
 #include "SimpleDelayPatch.hpp"
-
 #include "ParametricEqPatch.hpp"
 #include "OverdrivePatch.hpp"
 #include "PhaserPatch.hpp"
 #include "StateVariableFilterPatch.hpp"
 #include "ResonantFilterPatch.hpp"
-
 #include "LeakyIntegratorPatch.hpp"
-
 #include "FreeVerbPatch.hpp"
 #include "OctaveDownPatch.hpp"
-
 #include "StereoMixerPatch.hpp"
-#include "Contest/blo_bleep.hpp"
-#include "Contest/BiasPatch.hpp"
-#include "Contest/BitH8rPatch.hpp"
-#include "Contest/ConnyPatch.hpp"
-#include "Contest/DroneBox.hpp"
-#include "Contest/DualTremoloPatch.hpp"
 #include "VibroFlangePatch.hpp"
 #include "RingModulatorPatch.hpp"
 #include "SynthPatch.hpp"
 #include "FourBandsEqPatch.hpp"
 #include "EnvelopeFilterPatch.hpp"
 #include "JotReverbPatch.hpp"
+#include "Qompression.hpp"
+#include "PsycheFilter.hpp"
+#include "Contest/BiasedDelayPatch.hpp" 
+#include "Contest/blo_bleep.hpp"
+#include "Contest/BiasPatch.hpp"
+#include "Contest/BitH8rPatch.hpp"
+#include "Contest/ConnyPatch.hpp"
+#include "Contest/DroneBox.hpp"
+#include "Contest/DualTremoloPatch.hpp"
 #include "mdaPorts/MdaBandistoPatch.cpp"
 #include "mdaPorts/MdaStereoPatch.cpp"
 #include "mdaPorts/MdaTransientPatch.cpp"
-#include "Qompression.hpp"
-#include "PsycheFilter.hpp"
 
-#include "Contest/BiasedDelayPatch.hpp" 
+//#include "Autotalent/AutotalentPatch.hpp"
 
 // #include "EnvelopeFilterPatch.hpp" /* too cpu intensive (sqrt...) */
 // #include "TemplatePatch.hpp"
