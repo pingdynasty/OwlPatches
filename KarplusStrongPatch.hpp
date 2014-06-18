3cffc6689ebf0496bccfbab4eee863ac17d60125
mars <martin@rebel-it.com>
2014-06-18 17:22:08 +0100

    prefix defines

--- KarplusStrongPatch.hpp
+++ KarplusStrongPatch.hpp
@@ -18,8 +18,8 @@
 */
 /* 
    See http://www.lifeorange.com/SOFTWARE/audiocpptutorial.html
-   created by the OWL team 2014 */
-
+   Ported by the OWL team 2014
+*/
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -30,15 +30,15 @@
 #include "StompBox.h"
 
 // number of samples for delay line, 1000 gives us a min. possible freq of 44100/1000 = 29.4hz (sufficient)
-#define NUM_SAMPLES (1500)
-#define NUM_BUFFER (1<<16)
-#define NOISETYPE_GAUSSIAN 1
-#define NOISETYPE_RANDOM 2
+#define KP_NUM_SAMPLES (1500)
+#define KP_NUM_BUFFER (1<<16)
+#define KP_NOISETYPE_GAUSSIAN 1
+#define KP_NOISETYPE_RANDOM 2
 
 typedef struct {
   int numSamps; // N
-  float noise[NUM_SAMPLES];  // white noise x(n)
-  float pluck[NUM_BUFFER]; // output y(n)
+  float noise[KP_NUM_SAMPLES];  // white noise x(n)
+  float pluck[KP_NUM_BUFFER]; // output y(n)
   float amp;
   float duration;
   int phase;
@@ -60,10 +60,10 @@ public:
   }
 
   void processAudio(AudioBuffer &buffer){
-    data.numSamps = getParameterValue(PARAMETER_A) * (NUM_SAMPLES-8)+8;
+    data.numSamps = getParameterValue(PARAMETER_A) * (KP_NUM_SAMPLES-8)+8;
     data.amp = getParameterValue(PARAMETER_B);
     data.g = getParameterValue(PARAMETER_C)*(0.5-0.48)+0.48;
-    data.duration = getParameterValue(PARAMETER_D) * NUM_BUFFER;
+    data.duration = getParameterValue(PARAMETER_D) * KP_NUM_BUFFER;
 
     if(isButtonPressed(PUSHBUTTON))
       data.noteOn = true;
@@ -72,8 +72,6 @@ public:
     float* left = buffer.getSamples(0);
     float* right = buffer.getChannels() > 1 ? buffer.getSamples(1) : left;
     for(int i=0; i<size; ++i){
-      // put your code here!
-
       if(data.noteOn){
 	if(data.phase > (data.numSamps +  1)){
 	  // if we have filled up our delay line, y(n) = g * (y(n-N) + y( n-(N+1) ))
@@ -81,9 +79,9 @@ public:
 					      + data.pluck[data.phase - (data.numSamps + 1)] );
 	}else{
 	  // computing the first N samples, y(n) = x(n)
-	  if(data.noiseType == NOISETYPE_GAUSSIAN)
+	  if(data.noiseType == KP_NOISETYPE_GAUSSIAN)
 	    data.pluck[data.phase] = data.noise[data.phase]; // use gaussian white noise
-	  if(data.noiseType == NOISETYPE_RANDOM)
+	  if(data.noiseType == KP_NOISETYPE_RANDOM)
 	    data.pluck[data.phase] = rand()%100000/100000.;  // use random noise
 	}
 	left[i] = data.amp * data.pluck[data.phase];  // left channel
@@ -124,14 +122,14 @@ public:
 	/* random number in range 0 - 1 not including 1 */
 	float random = 0.f;
 
-	for (int i = 0; i < NUM_SAMPLES; i++){
+	for (int i = 0; i < KP_NUM_SAMPLES; i++){
 	  random = ((float)rand() / (float)RAND_MAX);
 	  data.noise[i] = (2.f * ((random * c2) + (random * c2) + (random * c2)) - 3.f * (c2 - 1.f)) * c3;	  
 	  if(fabs(data.noise[i]) > max)
 	    max = fabs(data.noise[i]);
 	}
 
-	for (int i = 0; i < NUM_SAMPLES; i++){
+	for (int i = 0; i < KP_NUM_SAMPLES; i++){
 	  //normalize the gain of our noise
 	  data.noise[i] = data.noise[i] / max;
 	}
