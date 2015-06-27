e5b5f8b9c1386e8787f848facff4c7d4bf53ec6e
mars <martin@rebel-it.com>
2015-06-27 09:59:23 +0100

    optimised code structure

--- mdaPorts/MdaStereoPatch.cpp
+++ mdaPorts/MdaStereoPatch.cpp
@@ -7,24 +7,11 @@
 
 #include "StompBox.h"
 
-#include <math.h>
-#include <float.h>
-
-
 class MdaStereoPatch : public Patch
 {
 private:
-    float fParam1;
-    float fParam2;
-    float fParam3;
-    float fParam4;
-    float fParam5;
-    float fParam6;
-
     float fli, fld, fri, frd, fdel, phi, dphi, mod;
-    int size, bufpos;
-    float oldParam1, oldParam2, oldParam3, oldParam4, oldParam5;
-    AudioBuffer *audiobuffer;
+    int bufpos;
     float *buffer;
 
 public:
@@ -37,78 +24,28 @@ public:
 	registerParameter(PARAMETER_D, "Balance");
 	registerParameter(PARAMETER_E, "Mod");
 
-	fParam1 = (float)0.78; //Haas/Comb width
-	fParam2 = (float)0.43; //delay
-	fParam3 = (float)0.50; //balance
-	fParam4 = (float)0.00; //mod
-	fParam5 = (float)0.50; //rate
-
-	size = 4800;
 	bufpos = 0;
-	audiobuffer = createMemoryBuffer(1, size);
+	AudioBuffer *audiobuffer = createMemoryBuffer(1, 4800);
 	buffer = audiobuffer->getSamples(0);
-
-	//calcs here!
-	phi=0;
-	dphi=(float)(3.141 * pow(10.0,-2.0 + 3.0 * fParam5) / getSampleRate());
-	mod=(float)(2100.0 * pow(fParam4, 2));
-
-	if(fParam1<0.5)
-	{
-	    fli = (float)(0.25 + (1.5 * fParam1));
-	    fld = 0.0;
-	    fri = (float)(2.0 * fParam1);
-	    frd = (float)(1.0 - fri);
-	}
-	else
-	{
-	    fli = (float)(1.5 - fParam1);
-	    fld = (float)(fParam1 - 0.5);
-	    fri = fli;
-	    frd = -fld;
-	}
-	fdel = (float)(20.0 + 2080.0 * pow(fParam2, 2));
-	if(fParam3>0.5)
-	{
-	    fli *= (float)((1.0 - fParam3) * 2.0);
-	    fld *= (float)((1.0 - fParam3) * 2.0);
-	}
-	else
-	{
-	    fri *= (2 * fParam3);
-	    frd *= (2 * fParam3);
-	}
-	fri *= (float)(0.5 + fabs(fParam1 - 0.5));
-	frd *= (float)(0.5 + fabs(fParam1 - 0.5));
-	fli *= (float)(0.5 + fabs(fParam1 - 0.5));
-	fld *= (float)(0.5 + fabs(fParam1 - 0.5));
     }
 
     void setParameter()
     {
+        float fParam1;
+	float fParam2;
+	float fParam3;
+	float fParam4;
+	float fParam5;
+	float fParam6;
 	fParam1 = getParameterValue(PARAMETER_A); // Comb Width
 	fParam2 = getParameterValue(PARAMETER_B); // Delay
 	fParam3 = getParameterValue(PARAMETER_D); // Balance
 	fParam5 = getParameterValue(PARAMETER_C); // Rate
 	fParam4 = getParameterValue(PARAMETER_E); // Mod
 
-	// Don't recalc if nothing has changed
-	if (oldParam1 == fParam1 &&
-	    oldParam2 == fParam2 &&
-	    oldParam3 == fParam3 &&
-	    oldParam4 == fParam4 &&
-	    oldParam5 == fParam5)
-	    return;
-
-	oldParam1 = fParam1;
-	oldParam2 = fParam2;
-	oldParam3 = fParam3;
-	oldParam4 = fParam4;
-	oldParam5 = fParam5;
-
 	//calcs here
-	dphi=(float)(3.141 * pow(10.0,-2.0 + 3.0 * fParam5) / getSampleRate());
-	mod=(float)(2100.0 * pow(fParam4, 2));
+	dphi=(float)(3.141 * powf(10.0,-2.0 + 3.0 * fParam5) / getSampleRate());
+	mod=(float)(2100.0 * powf(fParam4, 2));
 
 	if(fParam1<0.5)
 	{
@@ -146,85 +83,64 @@ public:
 	// Should free the buffer here ... if it ever gets called!
     }
 
-// process
-
     void processAudio(AudioBuffer &owlbuf)
     {
+	setParameter();
 	float *in1;
 	float *in2;
 	float *out1;
 	float *out2;
 	float a, b, c, d;
-	float li, ld, ri, rd, del, ph=phi, dph=dphi, mo=mod;
-	int tmp, bp = bufpos;
+	int tmp;
 	int sampleFrames = owlbuf.getSize();
 
-	if (owlbuf.getChannels() < 2) {  // Mono check
-	    in1 = owlbuf.getSamples(0); // L
-	    in2 = owlbuf.getSamples(0); // R
-	    out1 = owlbuf.getSamples(0); // L
-	    out2 = owlbuf.getSamples(0); // R
-	} else {
-	    in1 = owlbuf.getSamples(0); // L
-	    in2 = owlbuf.getSamples(1); // R
-	    out1 = owlbuf.getSamples(0); // L
-	    out2 = owlbuf.getSamples(1); // R
-	}
-
+	in1 = owlbuf.getSamples(0); // L
+	in2 = owlbuf.getSamples(1); // R
+	out1 = owlbuf.getSamples(0); // L
+	out2 = owlbuf.getSamples(1); // R
 	setParameter();
 
-	li = fli;
-	ld = fld;
-	ri = fri;
-	rd = frd;
-	del = fdel;
-
-	--in1;
-	--in2;
-	--out1;
-	--out2;
-	if(mo>0.f) //modulated delay
+	if(mod>0.f) //modulated delay
 	{
-	    while(--sampleFrames >= 0)
+	    while(sampleFrames-- > 0)
 	    {
-		a = *++in1 + *++in2; //sum to mono
+		a = *in1++ + *in2++; //sum to mono
 
-		*(buffer + bp) = a; //write
-		tmp = (bp + (int)(del + fabs(mo * sin(ph)) ) ) % 4410;
+		*(buffer + bufpos) = a; //write
+		tmp = (bufpos + (int)(fdel + fabs(mod * sin(phi)) ) ) % 4410;
 		b = *(buffer + tmp);
 
-		c = (a * li) - (b * ld); // output
-		d = (a * ri) - (b * rd);
+		c = (a * fli) - (b * fld); // output
+		d = (a * fri) - (b * frd);
 
-		bp = (bp - 1); if(bp < 0) bp = 4410; //buffer position
+		bufpos = (bufpos - 1); if(bufpos < 0) bufpos = 4410; //buffer position
 
-		ph = ph + dph;
+		phi = phi + dphi;
 
-		*++out1 = c;
-		*++out2 = d;
+		*out1++ = c;
+		*out2++ = d;
 	    }
 	}
 	else
 	{
-	    while(--sampleFrames >= 0)
+	    while(sampleFrames-- > 0)
 	    {
-		a = *++in1 + *++in2; //sum to mono
+		a = *in1++ + *in2++; //sum to mono
 
-		*(buffer + bp) = a; //write
-		tmp = (bp + (int)(del) ) % 4410;
+		*(buffer + bufpos) = a; //write
+		tmp = (bufpos + (int)(fdel) ) % 4410;
 		b = *(buffer + tmp);
 
-		c = (a * li) - (b * ld); // output
-		d = (a * ri) - (b * rd);
+		c = (a * fli) - (b * fld); // output
+		d = (a * fri) - (b * frd);
 
-		bp = (bp - 1); if(bp < 0) bp = 4410; //buffer position
+		bufpos = (bufpos - 1); if(bufpos < 0) bufpos = 4410; //buffer position
 
-		*++out1 = c;
-		*++out2 = d;
+		*out1++ = c;
+		*out2++ = d;
 	    }
 	}
-	bufpos = bp;
-	phi = (float)fmod(ph,6.2831853f);
+	phi = (float)fmod(phi,6.2831853f);
     }
 };
 #endif //__MdaStereoPatch_h__
