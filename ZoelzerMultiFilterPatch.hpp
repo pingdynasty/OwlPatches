6b389322e68d65e4911e3f1a7fcccf7a54e0801b
mars <martin@rebel-it.com>
2015-07-16 17:10:59 +0100

    moved coefficient calculations to BiquadFilter

--- ZoelzerMultiFilterPatch.hpp
+++ ZoelzerMultiFilterPatch.hpp
@@ -2,144 +2,64 @@
 #define __ZoelzerMultiFilterPatch_hpp__
 
 #include "StompBox.h"
-
-#define ZOELZER_LOWPASS_FILTER_MODE   0
-#define ZOELZER_HIGHPASS_FILTER_MODE  1
-#define ZOELZER_BANDPASS_FILTER_MODE  2
-#define ZOELZER_NOTCH_FILTER_MODE     3
-#define ZOELZER_PEAK_FILTER_MODE      4
-#define ZOELZER_LOWSHELF_FILTER_MODE  5
-#define ZOELZER_HIGHSHELF_FILTER_MODE 6
-#define ZOELZER_MODES                 7
 /**
 http://www.musicdsp.org/showArchiveComment.php?ArchiveID=37
 http://www.earlevel.com/main/2013/10/13/biquad-calculator-v2/
-
 Type : biquad IIR
 References : Udo Zoelzer: Digital Audio Signal Processing (John Wiley & Sons, ISBN 0 471 97226 6), Chris Townsend, Nigel Redmon
 */
 
-#include "BiquadFilter.hpp"
-
+// #define ZOELZER_FILTER_STAGES         2 // 4-pole, 24dB filter
+#define ZOELZER_FILTER_STAGES         4 // 8-pole, 48dB filter
+#define ZOELZER_LOWPASS_FILTER_MODE   0
+#define ZOELZER_HIGHSHELF_FILTER_MODE 1
+#define ZOELZER_PEAK_FILTER_MODE      2
+#define ZOELZER_LOWSHELF_FILTER_MODE  3
+#define ZOELZER_HIGHPASS_FILTER_MODE  4
+#define ZOELZER_NOTCH_FILTER_MODE     5
+#define ZOELZER_BANDPASS_FILTER_MODE  6
+#define ZOELZER_MODES                 5 // ignore notch and bandpass
 class ZoelzerMultiFilterPatch : public Patch {
 private:
-  BiquadFilter<1> previous;
-  BiquadFilter<1> filter;
-  float coeffs[5] = {0,0,0,0,0};
+  BiquadFilter* filter;
 public:
   ZoelzerMultiFilterPatch(){
     registerParameter(PARAMETER_A, "Mode");
     registerParameter(PARAMETER_B, "Frequency");
     registerParameter(PARAMETER_C, "Resonance");
     registerParameter(PARAMETER_D, "Gain");
-    previous.setCoefficents(coeffs);
+    filter = BiquadFilter::create(ZOELZER_FILTER_STAGES);
   }
   void processAudio(AudioBuffer &buffer){
     int mode = getParameterValue(PARAMETER_A)*ZOELZER_MODES;
-    float omega = (M_PI/2 - 0.01)*getParameterValue(PARAMETER_B) + 0.00001; // Frequency
-    float K = tan(omega);
-    float Q = getParameterValue(PARAMETER_C) * 10 + 0.1; // Resonance
+    float fc = getParameterValue(PARAMETER_B);
+    float q = getParameterValue(PARAMETER_C)*4+0.0001;
     float gain = getParameterValue(PARAMETER_D);
-    float V = abs(gain-0.5)*60 + 1; // Gain
-    float norm;
-    /* coeffs[b0, b1, b2, a1, a2] */
     switch(mode){
     case ZOELZER_LOWPASS_FILTER_MODE:
-      norm = 1 / (1 + K / Q + K * K);
-      coeffs[0] = K * K * norm;
-      coeffs[1] = 2 * coeffs[0];
-      coeffs[2] = coeffs[0];
-      coeffs[3] = 2 * (K * K - 1) * norm;
-      coeffs[4] = (1 - K / Q + K * K) * norm;
+      filter->setLowPass(fc, q);
       break;
     case ZOELZER_HIGHPASS_FILTER_MODE:
-      norm = 1 / (1 + K / Q + K * K);
-      coeffs[0] = 1 * norm;
-      coeffs[1] = -2 * coeffs[0];
-      coeffs[2] = coeffs[0];
-      coeffs[3] = 2 * (K * K - 1) * norm;
-      coeffs[4] = (1 - K / Q + K * K) * norm;
+      filter->setHighPass(fc, q);
       break;      
     case ZOELZER_BANDPASS_FILTER_MODE:
-      norm = 1 / (1 + K / Q + K * K);
-      coeffs[0] = K / Q * norm;
-      coeffs[1] = 0;
-      coeffs[2] = -coeffs[0];
-      coeffs[3] = 2 * (K * K - 1) * norm;
-      coeffs[4] = (1 - K / Q + K * K) * norm;
-      break;
+      filter->setBandPass(fc, q);
+      break;      
     case ZOELZER_NOTCH_FILTER_MODE:
-      norm = 1 / (1 + K / Q + K * K);
-      coeffs[0] = (1 + K * K) * norm;
-      coeffs[1] = 2 * (K * K - 1) * norm;
-      coeffs[2] = coeffs[0];
-      coeffs[3] = coeffs[1];
-      coeffs[4] = (1 - K / Q + K * K) * norm;
-      break;
+      filter->setNotch(fc, q);
+      break;      
     case ZOELZER_PEAK_FILTER_MODE:
-      if (gain >= 0.5) {
-	norm = 1 / (1 + 1/Q * K + K * K);
-	coeffs[0] = (1 + V/Q * K + K * K) * norm;
-	coeffs[1] = 2 * (K * K - 1) * norm;
-	coeffs[2] = (1 - V/Q * K + K * K) * norm;
-	coeffs[3] = coeffs[1];
-	coeffs[4] = (1 - 1/Q * K + K * K) * norm;
-      }
-      else {
-	norm = 1 / (1 + V/Q * K + K * K);
-	coeffs[0] = (1 + 1/Q * K + K * K) * norm;
-	coeffs[1] = 2 * (K * K - 1) * norm;
-	coeffs[2] = (1 - 1/Q * K + K * K) * norm;
-	coeffs[3] = coeffs[1];
-	coeffs[4] = (1 - V/Q * K + K * K) * norm;
-      }
-      break;
+      filter->setPeak(fc, q, gain);
+      break;      
     case ZOELZER_LOWSHELF_FILTER_MODE:
-      if (gain >= 0.5) {
-	norm = 1 / (1 + M_SQRT2 * K + K * K);
-	coeffs[0] = (1 + sqrt(2*V) * K + V * K * K) * norm;
-	coeffs[1] = 2 * (V * K * K - 1) * norm;
-	coeffs[2] = (1 - sqrt(2*V) * K + V * K * K) * norm;
-	coeffs[3] = 2 * (K * K - 1) * norm;
-	coeffs[4] = (1 - M_SQRT2 * K + K * K) * norm;
-      } else {
-	norm = 1 / (1 + sqrt(2*V) * K + V * K * K);
-	coeffs[0] = (1 + M_SQRT2 * K + K * K) * norm;
-	coeffs[1] = 2 * (K * K - 1) * norm;
-	coeffs[2] = (1 - M_SQRT2 * K + K * K) * norm;
-	coeffs[3] = 2 * (V * K * K - 1) * norm;
-	coeffs[4] = (1 - sqrt(2*V) * K + V * K * K) * norm;
-      }
+      filter->setLowShelf(fc, gain);
       break;
     case ZOELZER_HIGHSHELF_FILTER_MODE:
-      if (gain >= 0.5) {
-	norm = 1 / (1 + M_SQRT2 * K + K * K);
-	coeffs[0] = (V + sqrt(2*V) * K + K * K) * norm;
-	coeffs[1] = 2 * (K * K - V) * norm;
-	coeffs[2] = (V - sqrt(2*V) * K + K * K) * norm;
-	coeffs[3] = 2 * (K * K - 1) * norm;
-	coeffs[4] = (1 - M_SQRT2 * K + K * K) * norm;
-      } else {
-	norm = 1 / (V + sqrt(2*V) * K + K * K);
-	coeffs[0] = (1 + M_SQRT2 * K + K * K) * norm;
-	coeffs[1] = 2 * (K * K - 1) * norm;
-	coeffs[2] = (1 - M_SQRT2 * K + K * K) * norm;
-	coeffs[3] = 2 * (K * K - V) * norm;
-	coeffs[4] = (V - sqrt(2*V) * K + K * K) * norm;
-      }
-      break;
+      filter->setHighShelf(fc, gain);
+      break;      
     }
-    int size = buffer.getSize();
-    float* samples = buffer.getSamples(0);
-    float buf[size];
-    previous.process(samples, buf, size);
-    previous.setCoefficents(coeffs);
-    filter.setCoefficents(coeffs);
-    filter.process(samples, size);
-    for(int i=0; i<size; ++i){
-      float xfade = (float)i/(float)size;
-      samples[i] = buf[i]*(1.0f-xfade) + samples[i]*xfade;
-    }
+    FloatArray samples = buffer.getSamples(0);
+    filter->process(samples);
   }
 };
 
